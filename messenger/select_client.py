e0f982ea0ae921a2071588ef04f6838efb2becf8
Ivan Koposhilov <ikoposhilov@yandex.ru>
2023-04-25 21:47:03 +0500

    add listen and send

--- messenger/select_client.py
+++ messenger/select_client.py
@@ -19,13 +19,14 @@ def create_parser():
         parser = argparse.ArgumentParser()
         parser.add_argument ('-p', '--port', default=7777, help='TCP port for work, default 7777')
         parser.add_argument ('-a', '--addr', default='localhost', help='Listening IP, default localhost')
+        parser.add_argument('-m', '--mode', default='listen', nargs='?', help='Listen or send mode')
         return parser
     except Exception as e:
         logger.critical(e)
 
 
 
-def main(port, addr):
+def main(port, addr, mode):
 
     logger.info(f'Запущен клиент с парамертами: адрес сервера: {addr}, порт: {port}')
 
@@ -34,22 +35,25 @@ def main(port, addr):
     with socket(AF_INET, SOCK_STREAM) as sock: 
         sock.connect(ADDRESS) 
         while True:
-            message = input('Ваше сообщение: ')
-            msg = {
-                    "action": "msg",
-                    "time": datetime.now().strftime('%d.%m.%Y %H:%M:%S'),
-                    "to": "account_name",
-                    "from": "account_name",
-                    "encoding": "ascii",
-                    "message": message
-                    }
-            if msg == 'exit':
-                break
-            sock.send(str(msg).encode('utf-8')) 
-            data = sock.recv(1024).decode('utf-8')
-            data = eval(data)
-            answer = data['message']
-            print('Ответ:', answer)
+            if mode == 'send':
+                message = input('Ваше сообщение: ')
+                msg = {
+                        "action": "msg",
+                        "time": datetime.now().strftime('%d.%m.%Y %H:%M:%S'),
+                        "to": "account_name",
+                        "from": "account_name",
+                        "encoding": "ascii",
+                        "message": message
+                        }
+                if msg == 'exit':
+                    break
+                sock.send(str(msg).encode('utf-8'))
+                
+            if mode == 'listen':
+                data = sock.recv(1024).decode('utf-8')
+                data = eval(data)
+                answer = data['message']
+                print('Ответ:', answer)
 
 
 if __name__ == "__main__":
@@ -58,6 +62,7 @@ if __name__ == "__main__":
         namespace = parser.parse_args(sys.argv[1:])
         port = namespace.port
         addr = namespace.addr
-        main(port, addr)
+        mode = namespace.mode
+        main(port, addr, mode)
     except Exception as e:
         logger.critical(e)
