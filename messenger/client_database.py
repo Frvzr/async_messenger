919122f1a7f7dad4e2ee0474e9e5dc7bda6de11b
Ivan Koposhilov <ikoposhilov@yandex.ru>
2023-05-28 15:44:00 +0500

    homework_012 completed

---
+++ messenger/client_database.py
@@ -0,0 +1,117 @@
+from sqlalchemy import create_engine, Table, Column, Integer, String, Text, DateTime
+from sqlalchemy.orm import registry, sessionmaker
+import datetime
+
+
+class ClientDatabase:
+    class KnownUsers:
+        def __init__(self, user):
+            self.id = None
+            self.username = user
+
+    class MessageHistory:
+        def __init__(self, from_user, to_user, message):
+            self.id = None
+            self.from_user = from_user
+            self.to_user = to_user
+            self.message = message
+            self.date = datetime.datetime.now()
+
+    class Contacts:
+        def __init__(self, contact):
+            self.id = None
+            self.name = contact
+
+    def __init__(self, name):
+        self.database_engine = create_engine(f'sqlite:///client_{name}.db3', echo=False, pool_recycle=7200,
+                                             connect_args={'check_same_thread': False})
+
+        self.mapper_registry = registry()
+
+
+        users = Table('known_users', self.mapper_registry.metadata,
+                      Column('id', Integer, primary_key=True),
+                      Column('username', String)
+                      )
+
+
+        history = Table('message_history', self.mapper_registry.metadata,
+                        Column('id', Integer, primary_key=True),
+                        Column('from_user', String),
+                        Column('to_user', String),
+                        Column('message', Text),
+                        Column('date', DateTime)
+                        )
+
+
+        contacts = Table('contacts', self.mapper_registry.metadata,
+                         Column('id', Integer, primary_key=True),
+                         Column('name', String, unique=True)
+                         )
+
+        self.mapper_registry.metadata.create_all(self.database_engine)
+
+
+        self.mapper_registry.map_imperatively(self.KnownUsers, users)
+        self.mapper_registry.map_imperatively(self.MessageHistory, history)
+        self.mapper_registry.map_imperatively(self.Contacts, contacts)
+
+
+        Session = sessionmaker(bind=self.database_engine)
+        self.session = Session()
+
+        self.session.query(self.Contacts).delete()
+        self.session.commit()
+
+    def add_contact(self, contact):
+        if not self.session.query(self.Contacts).filter_by(name=contact).count():
+            contact_row = self.Contacts(contact)
+            self.session.add(contact_row)
+            self.session.commit()
+
+    def del_contact(self, contact):
+        self.session.query(self.Contacts).filter_by(name=contact).delete()
+
+    def add_users(self, users_list):
+        self.session.query(self.KnownUsers).delete()
+        for user in users_list:
+            user_row = self.KnownUsers(user)
+            self.session.add(user_row)
+        self.session.commit()
+
+    def save_message(self, from_user, to_user, message):
+        message_row = self.MessageHistory(from_user, to_user, message)
+        self.session.add(message_row)
+        self.session.commit()
+
+    def get_contacts(self):
+        return [contact[0] for contact in self.session.query(self.Contacts.name).all()]
+
+    def get_users(self):
+        return [user[0] for user in self.session.query(self.KnownUsers.username).all()]
+
+    def check_user(self, user):
+        if self.session.query(self.KnownUsers).filter_by(username=user).count():
+            return True
+        else:
+            return False
+
+    def check_contact(self, contact):
+        if self.session.query(self.Contacts).filter_by(name=contact).count():
+            return True
+        else:
+            return False
+
+    def get_history(self, from_who=None, to_who=None):
+        query = self.session.query(self.MessageHistory)
+        if from_who:
+            query = query.filter_by(from_user=from_who)
+        if to_who:
+            query = query.filter_by(to_user=to_who)
+        return [(history_row.from_user, history_row.to_user, history_row.message, history_row.date)
+                for history_row in query.all()]
+
+
+if __name__ == '__main__':
+    test_db = ClientDatabase('test1')
+    
---
+++ messenger/server_gui.py
@@ -0,0 +1,180 @@
+import sys
+from PyQt5.QtWidgets import QMainWindow, QAction, qApp, QApplication, QLabel, QTableView, QDialog, QPushButton, QLineEdit, QFileDialog, QMessageBox
+from PyQt5.QtGui import QStandardItemModel, QStandardItem
+from PyQt5.QtCore import Qt
+
+
+def gui_create_model(database):
+    list_users = database.active_users_list()
+    list = QStandardItemModel()
+    list.setHorizontalHeaderLabels(['Имя Клиента', 'IP Адрес', 'Порт', 'Время подключения'])
+    for row in list_users:
+        user, ip, port, time = row
+        user = QStandardItem(user)
+        user.setEditable(False)
+        ip = QStandardItem(ip)
+        ip.setEditable(False)
+        port = QStandardItem(str(port))
+        port.setEditable(False)
+        time = QStandardItem(str(time.replace(microsecond=0)))
+        time.setEditable(False)
+        list.appendRow([user, ip, port, time])
+    return list
+
+
+def create_stat_model(database):
+    hist_list = database.message_history()
+
+    list = QStandardItemModel()
+    list.setHorizontalHeaderLabels(
+        ['Имя Клиента', 'Последний раз входил', 'Сообщений отправлено', 'Сообщений получено'])
+    for row in hist_list:
+        user, last_seen, sent, recvd = row
+        user = QStandardItem(user)
+        user.setEditable(False)
+        last_seen = QStandardItem(str(last_seen.replace(microsecond=0)))
+        last_seen.setEditable(False)
+        sent = QStandardItem(str(sent))
+        sent.setEditable(False)
+        recvd = QStandardItem(str(recvd))
+        recvd.setEditable(False)
+        list.appendRow([user, last_seen, sent, recvd])
+    return list
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        exitAction = QAction('Выход', self)
+        exitAction.setShortcut('Ctrl+Q')
+        exitAction.triggered.connect(qApp.quit)
+
+        self.refresh_button = QAction('Обновить список', self)
+
+        self.config_btn = QAction('Настройки сервера', self)
+
+        self.show_history_button = QAction('История клиентов', self)
+
+
+        self.statusBar()
+
+        self.toolbar = self.addToolBar('MainBar')
+        self.toolbar.addAction(exitAction)
+        self.toolbar.addAction(self.refresh_button)
+        self.toolbar.addAction(self.show_history_button)
+        self.toolbar.addAction(self.config_btn)
+
+        self.setFixedSize(800, 600)
+        self.setWindowTitle('Messanger server')
+
+        self.label = QLabel('Список подключённых клиентов:', self)
+        self.label.setFixedSize(240, 15)
+        self.label.move(10, 25)
+
+        self.active_clients_table = QTableView(self)
+        self.active_clients_table.move(10, 45)
+        self.active_clients_table.setFixedSize(780, 400)
+
+        self.show()
+
+
+
+class HistoryWindow(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle('Статистика клиентов')
+        self.setFixedSize(600, 700)
+        self.setAttribute(Qt.WA_DeleteOnClose)
+
+        self.close_button = QPushButton('Закрыть', self)
+        self.close_button.move(250, 650)
+        self.close_button.clicked.connect(self.close)
+
+        self.history_table = QTableView(self)
+        self.history_table.move(10, 10)
+        self.history_table.setFixedSize(580, 620)
+
+        self.show()
+
+
+class ConfigWindow(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setFixedSize(365, 260)
+        self.setWindowTitle('Настройки сервера')
+
+        self.db_path_label = QLabel('Путь до файла базы данных: ', self)
+        self.db_path_label.move(10, 10)
+        self.db_path_label.setFixedSize(240, 15)
+
+        self.db_path = QLineEdit(self)
+        self.db_path.setFixedSize(250, 20)
+        self.db_path.move(10, 30)
+        self.db_path.setReadOnly(True)
+
+        self.db_path_select = QPushButton('Обзор...', self)
+        self.db_path_select.move(275, 28)
+
+        def open_file_dialog():
+            global dialog
+            dialog = QFileDialog(self)
+            path = dialog.getExistingDirectory()
+            path = path.replace('/', '\\')
+            self.db_path.insert(path)
+
+        self.db_path_select.clicked.connect(open_file_dialog)
+
+        self.db_file_label = QLabel('Имя файла базы данных: ', self)
+        self.db_file_label.move(10, 68)
+        self.db_file_label.setFixedSize(180, 15)
+
+        self.db_file = QLineEdit(self)
+        self.db_file.move(200, 66)
+        self.db_file.setFixedSize(150 , 20)
+
+        self.port_label = QLabel('Номер порта для соединений:', self)
+        self.port_label.move(10, 108)
+        self.port_label.setFixedSize(180, 15)
+
+        self.port = QLineEdit(self)
+        self.port.move(200, 108)
+        self.port.setFixedSize(150, 20)
+
+        self.ip_label = QLabel('С какого IP принимаем соединения:', self)
+        self.ip_label.move(10, 148)
+        self.ip_label.setFixedSize(180, 15)
+
+        self.ip_label_note = QLabel(' оставьте это поле пустым, чтобы\n принимать соединения с любых адресов.', self)
+        self.ip_label_note.move(10, 168)
+        self.ip_label_note.setFixedSize(500, 30)
+
+        self.ip = QLineEdit(self)
+        self.ip.move(200, 148)
+        self.ip.setFixedSize(150, 20)
+
+        self.save_btn = QPushButton('Сохранить' , self)
+        self.save_btn.move(190 , 220)
+
+        self.close_button = QPushButton('Закрыть', self)
+        self.close_button.move(275, 220)
+        self.close_button.clicked.connect(self.close)
+
+        self.show()
+
+
+if __name__ == '__main__':
+    
+    app = QApplication(sys.argv)
+    message = QMessageBox
+    dial = MainWindow()
+
+    app.exec_()
