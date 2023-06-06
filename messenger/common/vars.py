deb8ed6c6cda8ec020a8eca3bc745c8d26856ae7
Frvzr <ikoposhilov@yandex.ru>
2023-06-06 07:31:11 +0500

    fix

--- messenger/common/vars.py
+++ messenger/common/vars.py
@@ -1,61 +1,14 @@
-# import logging
-
-
-# DEFAULT_PORT = 7777
-# DEFAULT_IP_ADDRESS = '127.0.0.1'
-# MAX_CONNECTIONS = 5
-# MAX_PACKAGE_LENGTH = 1024
-# ENCODING = 'utf-8'
-# LOGGING_LEVEL = logging.DEBUG
-# SERVER_CONFIG = 'server.ini'
-
-# # Прококол JIM основные ключи:
-# ACTION = 'action'
-# TIME = 'time'
-# USER = 'user'
-# ACCOUNT_NAME = 'account_name'
-# SENDER = 'sender'
-# DESTINATION = 'destianation'
-
-
-# PRESENCE = 'presence'
-# RESPONSE = 'response'
-# ERROR = 'error'
-# MESSAGE = 'message'
-# MESSAGE_TEXT = 'text'
-# EXIT = 'exit'
-# GET_CONTACTS = 'get_contacts'
-# LIST_INFO = 'list_info'
-# REMOVE_CONTACT = 'remove'
-# ADD_CONTACT = 'add'
-# USERS_REQUEST = 'users_request'
-
-# RESPONSE_200 = {RESPONSE: 200}
-# RESPONSE_202 = {RESPONSE: 202,
-#                 LIST_INFO:None
-#                 }
-
-# RESPONSE_400 = {
-#             RESPONSE: 400,
-#             ERROR: None
-#         }
-
-
 import logging
 
-# Порт поумолчанию для сетевого ваимодействия
 DEFAULT_PORT = 7777
-# IP адрес по умолчанию для подключения клиента
 DEFAULT_IP_ADDRESS = '127.0.0.1'
-# Максимальная очередь подключений
+
 MAX_CONNECTIONS = 5
-# Максимальная длинна сообщения в байтах
 MAX_PACKAGE_LENGTH = 10240
-# Кодировка проекта
 ENCODING = 'utf-8'
-# Текущий уровень логирования
+
 LOGGING_LEVEL = logging.DEBUG
-# База данных для хранения данных сервера:
+
 SERVER_CONFIG = 'server.ini'
 
 # Прококол JIM основные ключи:
@@ -82,24 +35,18 @@ ADD_CONTACT = 'add'
 USERS_REQUEST = 'get_users'
 PUBLIC_KEY_REQUEST = 'pubkey_need'
 
-# Словари - ответы:
-# 200
+
 RESPONSE_200 = {RESPONSE: 200}
-# 202
 RESPONSE_202 = {RESPONSE: 202,
                 LIST_INFO: None
                 }
-# 400
+RESPONSE_205 = {
+    RESPONSE: 205
+}
 RESPONSE_400 = {
     RESPONSE: 400,
     ERROR: None
 }
-# 205
-RESPONSE_205 = {
-    RESPONSE: 205
-}
-
-# 511
 RESPONSE_511 = {
     RESPONSE: 511,
     DATA: None
